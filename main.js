6477788b59bb4a9a3009c748cfcfb41c5a4f39d1
Rene <rene@outlook.my>
2022-01-14 23:29:32 -0300

    added burger menu

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-const plantilla = (nombre, viewers, subscribers, photo, youtubeLink, youtubeChannel) => {
+const burgerMenu = document.getElementById('burger-menu')
+const showMenuId = document.getElementById('show-menu')
+
+const burgerOpen = document.getElementById('open')
+const burgerClose = document.getElementById('close')
+
+const showMenu = () => {
+    if (showMenuId.classList.contains('hidden')){
+        showMenuId.classList.remove('hidden')
+        showMenuId.classList.add('flex')
+        burgerClose.classList.remove('hidden')
+        burgerOpen.classList.add('hidden')
+    } else {
+        showMenuId.classList.remove('flex')
+        showMenuId.classList.add('hidden')
+        burgerOpen.classList.remove('hidden')
+        burgerClose.classList.add('hidden')
+    }
+}
+
+burgerMenu.onclick = showMenu
+
+const plantilla = (nombre, viewers, title, photo, youtubeLink, youtubeChannel) => {
     return `<div class="bg-gray-200 mx-2 my-2 border shadow-md rounded flex flex-row max-w-fit">
         <a class="hover:mouse-pointer shrink-0" href='${youtubeLink}' target='_blank'>
             <img class="rounded-l h-24 w-32 hover:opacity-70" src="${photo}" alt="vtuber's channel">
         </a>
         <div class="my-1 mx-2">
-            <a href='https://www.youtube.com/channel/${youtubeChannel}' target='_blank'><h2 class='hover:text-blue-600 hover:underline hover:decoration-blue-500'>${nombre}</h2></a>
+            <a href='${youtubeLink}' target='_blank'><h2 class='text-sm tracking-tight hover:text-blue-600 hover:underline hover:decoration-blue-500'>${title}</h2></a>
             <div class="flex items-center text-sm text-gray-600">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
@@ -15,7 +37,7 @@ const plantilla = (nombre, viewers, subscribers, photo, youtubeLink, youtubeChan
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <p> <span class='text-blue-700 font-semibold font-mono'>${subscribers}</span> Subscribers</p>
+                <a href='https://www.youtube.com/channel/${youtubeChannel}' target='_blank' class='text-blue-700 font-semibold font-mono tracking-tighter'>${nombre}</a>
             </div>
         </div>
     </div>`
@@ -27,7 +49,7 @@ const plantillaStats = (live, ended) => `
 `
 
 const plantillaTalent = (name, subscribers, photo) => `<div class="flex flex-col">
-<img class="object-cover h-16 w-16" src="${photo}" alt="prueba">
+<img class="object-cover h-16 w-16 rounded" src="${photo}" alt="prueba">
 <div class="text-sm text-center mt-1">
     <div class="flex items-center">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -63,10 +85,10 @@ vtubers().then( value => {
             const youtubeLink = `https://www.youtube.com/watch?v=${youtube}`
 
 
-            createDiv.innerHTML += plantilla(name, viewers, subscribers, thumbnail, youtubeLink, youtubeChannel)
+            createDiv.innerHTML += plantilla(name, viewers, title, thumbnail, youtubeLink, youtubeChannel)
         }
     })
-})
+}).then( nada => document.querySelectorAll('[id=loading-spin]').forEach( spin => spin.classList.add('hidden')))
 
 const videos = async() => {
 	const URL = 'https://api.holotools.app/v1/videos/';
@@ -90,10 +112,9 @@ channels().then( value => {
         const { photo, subscriber_count:subscribers, name } = element
         talentCorrusel.innerHTML += plantillaTalent(name, subscribers, photo)
     })
-
 })
 
 const warning = document.getElementById('warning')
 const warningButton = document.getElementById('warningButton')
 const eraseWarning = () => warning.classList.add('hidden')
-warningButton.onclick = eraseWarning 
+warningButton.onclick = eraseWarning
